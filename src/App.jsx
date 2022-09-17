3cc51270e336c819933ef43a22fdeec54f205c27
Alok Kumar <71998637+alokkumar-dev@users.noreply.github.com>
2022-09-17 21:26:00 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Forms } from "./components/Form";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Forms />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import "./Styles/Main.css"
+export const Header = ()=>{
+    return(
+        <header>
+            <h2>Toggle Task</h2>
+        </header>
+    )
+}
